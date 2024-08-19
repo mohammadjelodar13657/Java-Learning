10613ab38c2a56c382754b963653318716cab551
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 02:41:40 +0330

    programming exercise 1 done

---
+++ src/chapter1/exercises/PE1.java
@@ -0,0 +1,9 @@
+package chapter1.exercises;
+
+public class PE1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
