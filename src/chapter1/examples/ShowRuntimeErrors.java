8147094da9dab0201e4cf56fef964b97ccc5ab27
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 02:34:32 +0330

    example 5 done

---
+++ src/chapter1/examples/ShowRuntimeErrors.java
@@ -0,0 +1,8 @@
+package chapter1.examples;
+
+public class ShowRuntimeErrors {
+    public static void main(String[] args) {
+        // divide by zero is runtime error
+        System.out.println(1 / 0);
+    }
+}
