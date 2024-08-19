00bb9be8688d7adbe11bc4050ac34ce639c2cd9e
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 03:13:42 +0330

    programming exercise 9 done

---
+++ src/chapter1/exercises/PE9.java
@@ -0,0 +1,10 @@
+package chapter1.exercises;
+
+public class PE9 {
+    public static void main(String[] args) {
+        System.out.print("The area of a rectangle with width 4.5 and length 7.9 is: ");
+        System.out.println(4.5 * 7.9);
+        System.out.print("The perimeter of a rectangle with width 4.5 and length 7.9 is: ");
+        System.out.println(2 * (4.5 + 7.9));
+    }
+}
