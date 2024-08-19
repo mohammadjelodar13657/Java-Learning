9a7b44c96d4f374f40fc4e54ea43df8c06d99b22
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 02:32:33 +0330

    example 4 done

---
+++ src/chapter1/examples/ShowSyntaxErrors.java
@@ -0,0 +1,8 @@
+package chapter1.examples;
+
+public class ShowSyntaxErrors {
+    // void keyword is missed
+//    public static main(String[] args) {
+//        System.out.println("Welcome to Java");
+//    }
+}
