e2cdd5c3a7aed6bce9dbe0b98cee189e69f7e213
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 03:19:30 +0330

    programming exercise 10 done

---
+++ src/chapter1/exercises/PE10.java
@@ -0,0 +1,10 @@
+package chapter1.exercises;
+
+public class PE10 {
+    public static void main(String[] args) {
+        System.out.println("A runner runs 14 kilometers in 45 minutes and 30 seconds.");
+        System.out.print("His average speed is ");
+        System.out.print((14 / 1.6) / ((45 * 60 + 30) / 3600.0));
+        System.out.println(" miles/hours");
+    }
+}
