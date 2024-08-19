0d5c16325fbc5faee2a2f6f63481f0da21f19ab7
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 03:06:26 +0330

    programming exercise 7 done

---
+++ src/chapter1/exercises/PE7.java
@@ -0,0 +1,10 @@
+package chapter1.exercises;
+
+public class PE7 {
+    public static void main(String[] args) {
+        System.out.print("4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) = ");
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+        System.out.print("4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1.0/13) = ");
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+    }
+}
