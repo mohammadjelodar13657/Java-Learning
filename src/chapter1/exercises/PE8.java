33dbf947f1150f9db16a1b064b1fd8bb0d14af44
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 03:10:05 +0330

    programming exercise 8 done

---
+++ src/chapter1/exercises/PE8.java
@@ -0,0 +1,10 @@
+package chapter1.exercises;
+
+public class PE8 {
+    public static void main(String[] args) {
+        System.out.print("The area of a circle with radius 5.5 is: ");
+        System.out.println(5.5 * 5.5 * 3.14);
+        System.out.print("The perimeter of a circle with radius 5.5 is: ");
+        System.out.println(2 * 5.5 * 3.14);
+    }
+}
