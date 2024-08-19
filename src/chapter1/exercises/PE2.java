4770f5a5d929bcc3d5679dca2ea843b68f3976bd
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 02:42:46 +0330

    programming exercise 2 done

---
+++ src/chapter1/exercises/PE2.java
@@ -0,0 +1,11 @@
+package chapter1.exercises;
+
+public class PE2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+    }
+}
