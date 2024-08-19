b395ee6ac49dc36d95fddc216dc3ff11c4532b1d
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 02:58:18 +0330

    programming exercise 5 done

---
+++ src/chapter1/exercises/PE5.java
@@ -0,0 +1,8 @@
+package chapter1.exercises;
+
+public class PE5 {
+    public static void main(String[] args) {
+        System.out.println("(9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5) = ");
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
