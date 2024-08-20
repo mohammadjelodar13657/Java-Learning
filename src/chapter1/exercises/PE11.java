9256be6e04349de799e96780e517da6ad9d49c91
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-20 03:34:49 +0330

    programming exercise 11 done

---
+++ src/chapter1/exercises/PE11.java
@@ -0,0 +1,30 @@
+package chapter1.exercises;
+
+public class PE11 {
+    public static void main(String[] args) {
+        System.out.print("Population after 1 year is ");
+        System.out.print(312032486 + (365 * 24 * 35600 / 7)
+                - (365 * 24 * 3600 / 13) - (365 * 24 * 3600 / 45));
+        System.out.println(" persons.");
+
+        System.out.print("Population after 2 year is ");
+        System.out.print(312032486 + (2 * 365 * 24 * 35600 / 7)
+                - (2 * 365 * 24 * 3600 / 13) - (2 * 365 * 24 * 3600 / 45));
+        System.out.println(" persons.");
+
+        System.out.print("Population after 3 year is ");
+        System.out.print(312032486 + (3 * 365 * 24 * 35600 / 7)
+                - (3 * 365 * 24 * 3600 / 13) - (3 * 365 * 24 * 3600 / 45));
+        System.out.println(" persons.");
+
+        System.out.print("Population after 4 year is ");
+        System.out.print(312032486 + (4 * 365 * 24 * 35600 / 7)
+                - (4 * 365 * 24 * 3600 / 13) - (4 * 365 * 24 * 3600 / 45));
+        System.out.println(" persons.");
+
+        System.out.print("Population after 5 year is ");
+        System.out.print(312032486 + (5 * 365 * 24 * 35600 / 7)
+                - (5 * 365 * 24 * 3600 / 13) - (5 * 365 * 24 * 3600 / 45));
+        System.out.println(" persons.");
+    }
+}
