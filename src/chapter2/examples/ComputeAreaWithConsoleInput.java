2078dcf7cad76cb978d0ce9eee97a0799b98b63a
mohammad jelodar <mohammad.jelodar@gmail.com>
2024-08-22 02:08:28 +0330

    Example 2.2 done

---
+++ src/chapter2/examples/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,23 @@
+package chapter2.examples;
+
+// Listing 2.2
+
+import java.util.Scanner;
+
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        // Create a scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a radius
+        System.out.println("Enter a number for radius: ");
+        double radius = input.nextDouble();
+
+        // Compute area
+        double area = radius * radius * 3.14159;
+
+        // Display results
+        System.out.println("The area for the circle of radius " +
+                radius + " is " + area);
+    }
+}
